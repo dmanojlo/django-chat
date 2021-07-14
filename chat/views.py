7325b4db86d8ddcd2346dc8297a9b28842556759
dmanojlo <dragojlo@net.hr>
2021-07-14 17:13:35 +0200

    Initial

--- chat/views.py
+++ chat/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
-    return render(request, 'chat/chatroom.html', {'room_name': room_name})
+    username = request.GET.get('username', 'Anonymus')
+    return render(request, 'chat/chatroom.html', {'room_name': room_name, 'username':username})
