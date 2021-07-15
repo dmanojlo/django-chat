0649fa34c193645d3293dbc861b989a26992ac3d
dmanojlo <dragojlo@net.hr>
2021-07-15 17:50:27 +0200

    Initial

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import RoomForm
+from .models import Room
 # Create your views here.
 
 def index(request):
-    return render(request, 'chat/index.html', {})
+    form = RoomForm(request.POST or None)
+    if form.is_valid():
+        room_model = form.save()
+        return redirect('chat:user', room_model.pk)
+    context = { 'form': form }
+    return render(request, 'chat/index.html', context)
 
 def room(request, room_name):
     username = request.GET.get('username', 'Anonymus')
     return render(request, 'chat/chatroom.html', {'room_name': room_name, 'username':username})
+
+def user(request,pk):
+    room = Room.objects.get(pk=pk)
+    return render(request, 'chat/room.html', {'room_name':room.room_name})
